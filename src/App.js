b264768188df9c1762ef85ae6304d12656d8468a
Carl Jordan Duff <jordan.duff@student.awesomeinc.org>
2022-03-17 23:44:19 +0000

    created appetizers

--- src/App.js
+++ src/App.js
@@ -3,18 +3,22 @@ import Container from "react-bootstrap/Container";
 import { Outlet, Link } from "react-router-dom";
 import "./App.css";
 import "./index.css";
+import logo from './images/logo.png'
 
 export default function App() {
   return (
     <div>
     <Container className="p-5 mb-4 rounded-3 cc">
-    <h1 className="header">Restaurant Name</h1>
+      
+    
+    <img className='logo' src={logo} alt="Logo"/>
     <div className='nav'>
-      <Link to="/" className="menu-link">Home</Link> {" "}
-      <Link to="/menu" className="menu-link">Menu</Link> {" "}
+      <Link to="/" style={{ textDecoration: 'none' }} className="menu-link">Home</Link> {" "}
+      <Link to="/menu" style={{ textDecoration: 'none' }} className="menu-link">Menu</Link> {" "}
       </div>
   </Container>
    
+      
 
 
 
--- src/routes/appetizers.js
+++ src/routes/appetizers.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { getData } from '../util/data';
-import Axios from 'axios'
 import Card from 'react-bootstrap/Card'
+import ListGroup from 'react-bootstrap/ListGroup'
+
 
 export default function Appetizers() {
     //Declaring a state variable called apps and set it to an empty array 
@@ -9,8 +10,7 @@ export default function Appetizers() {
   // During the initial render, the returned state (apps) is the same as the value passed as the first argument ([])
   // The setApps function is used to update the state. It accepts a new state value and enqueues a re-render of the component.
 
-    const [apps, setApps] = useState([]); //returns a pair
-    // {apps.filter(app => app.category.title === 'Appetizer')}
+    const [apps, setApps] = useState([]); 
 
     useEffect(() => {
         getData()
@@ -25,7 +25,7 @@ export default function Appetizers() {
         <div>
         {
         apps
-        .filter(app => app.category.title === 'Appetizer')
+        .filter(app => app.category.title === 'Sandwiches')
         .map(app => <Appetizer key={app.id} app={app}/>)}
         </div>
     );
@@ -33,15 +33,48 @@ export default function Appetizers() {
 }
     const Appetizer = ({app}) => {
         return (
-            <Card className='cards justify-content-center' style={{ width: '18rem' }}>
-  <Card.Img variant="top" />
-  <Card.Body>
-    <Card.Title>{app.title}</Card.Title>
-    <Card.Text>
-      {app.description}
-    </Card.Text>
-  </Card.Body>
-</Card>
+          
+        //     <div class='container'>
+        //         <div class='row'>
+        //             <div class='col'>
+
+        //     <Card className='cards' style={{ width: '18rem' }}>
+        //     <Card.Body>
+        //       <Card.Title className='title'>{app.title}</Card.Title>
+        //       <Card.Subtitle className="mb-2 price text-muted">{app.price}</Card.Subtitle>
+        //       <Card.Text className='card-body'>
+        //         {app.description}
+        //       </Card.Text>
+        //     </Card.Body>
+        //   </Card>
+        //             </div>
+
+        //         </div>
+        //   </div>
+            
+            <div className='small'>
+                <div className='menu-container'>
+                    <div className="row">
+                <h3 className='title'>{app.title}</h3>
+                <h6 className='price'>{app.price}</h6> 
+                <div className='p-container des'>
+                <p className='description p-container'> {app.description}</p>
+                    </div>
+                </div>
+                </div>
+                </div>
+            
+            
+
+//             <Card className='cards justify-content-center' style={{ width: '18rem' }}>
+//   <Card.Img variant="top" />
+//   <Card.Body>
+//     <Card.Title>{app.title}</Card.Title>
+//     <Card.Text>
+//       {app.description}
+//     </Card.Text>
+//   </Card.Body>
+// </Card>
         
         )
     } 
